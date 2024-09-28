3783fb8a383534277ec23e35dea58b6a31c89236
gtcelvam <selvasizzler@gmail.com>
2024-09-28 11:52:14 +0530

    skeleton height updated.

--- components/groups/skeleton/cardSkeleton.tsx
+++ components/groups/skeleton/cardSkeleton.tsx
@@ -18,8 +18,8 @@ const SkeletonCardList: FC<SkeletonCardListProps> = (props) => {
 function SkeletonCard() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl bg-slate-300" />
-      <div className="space-y-2">
+      <Skeleton className="h-[150px] w-[250px] rounded-xl bg-slate-300" />
+      <div className="space-y-2 h-[120px]">
         <Skeleton className="h-4 w-[250px] bg-slate-300" />
         <Skeleton className="h-4 w-[200px] bg-slate-300" />
       </div>
